e1af39d35dfdab7a46b077ec9e2d6307a779ce91
Aleksandra Popova <alexnika@bk.ru>
2022-05-17 17:09:40 +0300

    added task_4

---
+++ tasks/task_4/Program.cs
@@ -0,0 +1,24 @@
+﻿/// Написать программу вычисления произведения чисел от 1 до N
+/*Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+int number1 = 1;
+int product = 1;
+Console.Write($"Product numbers from 1 to {number}: ");
+while (number1 <= number) 
+{
+    product = product * number1;
+    number1++;
+}
+Console.WriteLine($"{product} ");*/
+
+
+Console.Write("Введите число n:");
+int n = int.Parse(Console.ReadLine());
+int p = 1;
+for (int i = 1; i <= n; i++)
+{
+    p = p*i;
+}
+Console.WriteLine($"Произведение чисел от 1 до {n} = {p}");
+
+
